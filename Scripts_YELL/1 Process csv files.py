ec5daaff41cf8d43d5bf35315bf168ef3121f821
Rudolph <RRudolph@nps.gov>
2020-03-31 17:24:11 -0700

    added YELL python scripts

---
+++ Scripts_YELL/1 Process csv files.py
@@ -0,0 +1,47 @@
+# Takes all csv files in a folder and strips out the junk at the header of the file.
+# Puts them all int a new folder named by the well name and the date of the folder.
+# It's important to have the files in a YYYY_MM format so the script can pull that 
+# and attach it to the file name. 
+
+import os, glob, re, sys
+
+inPath = r"C:\Temp\Yellowstone wetland\PilotWells_YELL NR wells\Site Data"
+outPath = r"C:\Temp\Yellowstone wetland\PilotWells_YELL NR wells\Processed Data"
+searchFiles = (".csv") 
+
+def walk_dir(dir_):
+    """
+    A directory walking generator. 
+	Use: files = walk_dir(some_dir)
+	Then for loop over files.
+    """
+    for root, dirs, files in os.walk(dir_):
+        files = [ fi for fi in files if fi.endswith(searchFiles)]
+        for file in files:
+        	yield os.path.join(root, file)
+
+files = walk_dir(inPath)
+
+pattern = r"(NR-)(\w{2}\d+)"
+
+for file in files:
+	if "BaroMerge" in file:
+		print("Found valid csv file: " + file)
+		wellSearch = re.search(pattern, file)
+
+		well = wellSearch.group()
+		print("Well: {}".format(well))
+		# fileDate = file.split("\\")[-2]
+
+		outFile = outPath + os.sep + well + ".csv"
+
+		print("Processing: " + outFile)
+
+		# Open a csv, start writing output when it hits "Date and Time,Seconds"
+		with open(file, 'r') as infile, open(outFile, 'w') as outfile:
+		    # Read the file line by line...
+		    for line in iter(infile.readline, ''):
+		        if "Date and Time,Seconds" in line:
+		            outfile.write(line)
+		            outfile.write(infile.read())
+		            break
